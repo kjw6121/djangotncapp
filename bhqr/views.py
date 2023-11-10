2ddc45a48857ca59382a2d37f0bf52a02e10212c
kjw6121 <technet4111@gmail.com>
2023-11-10 16:42:37 +0900

    202311105999

--- bhqr/views.py
+++ bhqr/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import HttpResponse, render
 from django.views import generic
+from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 
-
 def index(request):
-    return render(request, 'bhqr.html')
+    current_user = request.user
+    current_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render(request, 'bhqr.html', {'current_user': current_user, 'current_time': current_time})
 
 
 def error_page(request):
