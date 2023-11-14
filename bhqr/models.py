5d1f606a9de1a006ad682cd3d4dda0143c06b4b7
kjw6121 <technet4111@gmail.com>
2023-11-14 16:52:18 +0900

    2013111402

--- bhqr/models.py
+++ bhqr/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class ScanData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    scan_message = models.CharField(max_length=255)
+    scan_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.scan_message} - {self.scan_time}"
--- models.py
+++ models.py
@@ -159,13 +159,3 @@ class PollsBlog(models.Model):
 
 # models.py
 
-from django.db import models
-from django.contrib.auth.models import User
-
-class ScanData(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    scan_message = models.CharField(max_length=255)
-    scan_time = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.scan_message} - {self.scan_time}"
