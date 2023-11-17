448599c4177985fdf36056b2dfc04b3acda852b3
kjw6121 <technet4111@gmail.com>
2023-11-17 12:05:14 +0900

    2023111710

--- mysite/views.py
+++ mysite/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
 
 
 def signup(request):
@@ -22,7 +26,7 @@ def signup(request):
     return render(request, 'signup.html', context)
 
 
-
+@login_required(login_url='login')  # 로그인되어 있지 않을 때 리다이렉트할 URL
 def main(request):
     return render(request, 'main.html')
 
