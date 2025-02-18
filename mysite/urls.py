907ce3c11dbb47dd47d5658330c5a6a687d0ffee
kjw6121 <technet4111@gmail.com>
2025-02-18 12:53:57 +0900

    recognition0.4

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('bhqr/', include('bhqr.urls')),
     path('outbound/', include('outbound.urls')),
-    path('outbound/', include('image_recognition.urls')),
+    path('image_recognition/', include('image_recognition.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
