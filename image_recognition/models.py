33b28f4adb039bfe465285b0b073b28a0a0b38ff
kjw6121 <technet4111@gmail.com>
2025-02-18 12:48:25 +0900

    recognition0.3

--- image_recognition/models.py
+++ image_recognition/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='')
     uploaded_at = models.DateTimeField(auto_now_add=True)
